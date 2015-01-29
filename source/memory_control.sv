f0035a20add3b5158c302796af3baed157d919c6
Sheik Dawood <dawood0@purdue.edu>
2015-01-29 18:21:17 -0500

    thu 29

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -1,10 +1,10 @@
 /*
-  Eric Villasenor
-  evillase@gmail.com
+ Sheik Dawood
+ dawood0@purdue.edu
 
-  this block is the coherence protocol
-  and artibtration for ram
-*/
+ this block is the coherence protocol
+ and artibtration for ram
+ */
 
 // interface include
 `include "cache_control_if.vh"
@@ -13,13 +13,26 @@
 `include "cpu_types_pkg.vh"
 
 module memory_control (
-  input CLK, nRST,
-  cache_control_if.cc ccif
-);
-  // type import
-  import cpu_types_pkg::*;
+		       input CLK, nRST,
+		       cache_control_if.cc ccif
+		       );
+   // type import
+   import cpu_types_pkg::*;
+   // number of cpus for cc
+   parameter CPUS = 2;
 
-  // number of cpus for cc
-  parameter CPUS = 2;
+   always_ff @(posedge CLK or negedge nRST)
+     begin
+	if(!nRST)
+	  begin
+	     iwait <= 1'b0;
+	     dwait <= 1'b0;
+	     iload <= 1'b0;
+	     dload <= 1'b0;
+	     
+	  end
+	else
+     end
+   
 
 endmodule
