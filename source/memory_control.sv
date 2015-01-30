eb267a256062a2d4e2b524e7ca09f69c1b4919ef
Everett Berry <epberry@purdue.edu>
2015-01-30 02:21:16 -0500

    working on memory control

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,32 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 2;
 
+  always_comb
+  begin
+    // an instruction is being fetched
+    if (ccif.iREN[CPUID] && ccif.ramstate == ACCESS) ccif.iwait = 0;
+    else ccif.iwait = 1;
+
+    // data is being fetched
+    if ((ccif.dREN[CPUID] || ccif.dWEN[CPUID]) && ccif.ramstate == ACCESS)
+      ccif.dwait = 0;
+    else ccif.dwait = 1;
+
+    // find the address being read or written
+    if (ccif.dREN[CPUID] || ccif.dWEN[CPUID])
+      ccif.ramaddr = ccif.daddr[CPUID];
+    else ccif.ramaddr = ccif.iaddr[CPUID];
+
+    // is the ram being read or written?
+    if (ccif.dREN[CPUID] || ccif.iREN[CPUID]) ccif.ramREN = 1;
+    else ccif.ramREN = 0;
+
+  end
+
+  // simply output these signals from the interface
+  assign ccif.ramstore = ccif.dstore[CPUID];
+  assign ccif.iload = ccif.ramload;
+  assign ccif.dload = ccif.ramload;
+  assign ccif.ramWEN = ccif.dWEN[CPUID];
+
 endmodule
