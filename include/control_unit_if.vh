4c799af0310fc388287d4678b44577f68abf096c
Everett Berry <epberry@purdue.edu>
2015-02-06 03:20:36 -0500

    possibly correct control unit

--- include/control_unit_if.vh
+++ include/control_unit_if.vh
@@ -17,28 +17,74 @@ interface control_unit_if;
   word_t instr;
   aluop_t ALUop;
 
-  logic extOp, ALUsrc, lui, shift; // ALU related instructions
+  logic ext, alusrc, lui, shift; // ALU related instructions
   logic memwr, memread, memtoreg, regwr, regdst; // move data around
-  logic beq, bne, jumpi, jumpreg, jumpal; // jump and branch
+  logic branch, jumpi, jumpreg, jumpal; // jump and branch
   logic [2:0] PCsrc; // several options for next addr
   logic halt;
 
   modport control (
     input instr,
-    output extOp, ALUsrc, lui, shift,
+    output ext, alusrc, lui, shift,
     memwr, memread, memtoreg, regwr, regdst,
     beq, bne, jumpi, jumpreg, jumpal, halt,
     output PCsrc
   );
 
-  modport tb (
-    output instr,
-    input extOp, ALUsrc, lui, shift,
-    memwr, memread, memtoreg, regwr, regdst,
-    beq, bne, jumpi, jumpreg, jumpal, halt,
-    input PCsrc
-  );
-
 endinterface
 
 `endif // DATAPATH_IF_VH
+
+/*
+
+ assign cuif.regdest = (opcode == RTYPE);
+assign cuif.halt = (opcode == HALT);
+assign cuif.memread = (opcode == LW);
+assign cuif.memwrite = (opcode == SW);
+assign cuif.jal = (opcode == JAL);
+assign cuif.regwrite = ~((func == JR) | (opcode == BEQ) | (opcode == BNE) |
+(opcode == SW) | (opcode == J) | (opcode == HALT));
+assign cuif.lui = (opcode == LUI);
+assign cuif.alusrc = (opcode == ADDIU) | (opcode == ANDI) | (opcode == LUI) |
+(opcode == LW) | (opcode == ORI) | (opcode == SLTI) | (opcode == SLTIU) |
+(opcode == SW) | (opcode == LL) | (opcode == SC) | (opcode == XORI);
+assign cuif.bne = (opcode == BNE);
+assign cuif.memtoreg = (opcode == LW);
+assign cuif.branch = (opcode == BEQ) | (opcode == BNE);
+assign cuif.jump = (opcode == J) | (opcode == JAL);
+assign cuif.jr = (opcode == RTYPE) & (func == JR);
+assign cuif.extendtype = ~((opcode == ANDI) | (opcode == LUI) | (opcode == ORI) |
+(opcode == XORI));
+assign cuif.shift = (opcode == RTYPE) & ((func == SLL) | (func == SRL));
+
+// alu op development
+aluop_t opfromfunct;
+always_comb begin
+casez (func)
+SLL: opfromfunct = ALU_SLL;
+SRL: opfromfunct = ALU_SRL;
+ADD, ADDU: opfromfunct = ALU_ADD;
+SUB, SUBU: opfromfunct = ALU_SUB;
+AND: opfromfunct = ALU_AND;
+OR: opfromfunct = ALU_OR;
+XOR: opfromfunct = ALU_XOR;
+NOR: opfromfunct = ALU_NOR;
+SLT: opfromfunct = ALU_SLT;
+SLTU: opfromfunct = ALU_SLTU;
+default: opfromfunct = aluop_t'('1);
+endcase
+
+casez (opcode)
+RTYPE: cuif.aluop = opfromfunct;
+ADDIU, LW, SW, LL, SC: cuif.aluop = ALU_ADD;
+ANDI: cuif.aluop = ALU_AND;
+ORI, LUI: cuif.aluop = ALU_OR;
+BEQ, BNE: cuif.aluop = ALU_SUB;
+SLTI: cuif.aluop = ALU_SLT;
+SLTIU: cuif.aluop = ALU_SLTU;
+XOR: cuif.aluop = ALU_XOR;
+default: cuif.aluop = aluop_t'('1);
+endcase
+end
+
+/*
--- source/control_unit.sv
+++ source/control_unit.sv
@@ -6,10 +6,139 @@
 */
 
 `include "cpu_types_pkg.vh"
+`include "control_unit_if.vh"
 
 module control_unit (
-
+  input logic CLK, nRST,
+  control_unit_if.control cuif
 );
 
+  import cpu_types_pkg::*;
+
+  opcode_t opcode;
+  funct_t func;
+
+  // cast instruction to take advantage of cpu types
+  opcode = opcode_t'(cuif.instr[31:26]);
+  func = funct_t'(cuif.instr[5:0]);
+
+  always_comb
+  begin
+    // Control signals which default to 0
+    cuif.alusrc = 1'b0; cuif.lui = 1'b0; cuif.shift = 1'b0;
+    cuif.memwr = 1'b0; cuif.memread = 1'b0; cuif.memtoreg = 1'b0;
+    cuif.regdst = 1'b0; cuif.halt = 1'b0; cuif.jal = 1'b0;
+    cuif.lui = 1'b0; cuif.alusrc = 1'b0; cuif.bne = 1'b0;
+    cuif.branch = 1'b0; cuif.jump = 1'b0; cuif.jumpr = 1'b0;
+
+    // Control signals which default to 1
+    cuif.regwr = 1'b1; cuif.ext = 1'b1;
+
+    // Other default
+    cuif.aluop = aluop_t'('1);
+
+    casez (opcode)
+      /*********** R-type ******************/
+      RTYPE:
+      begin
+        cuif.regdst = 1'b1;
+        casez(func)
+          ADDU, ADD: cuif.aluop = ALU_ADD;
+          AND: cuif.aluop = ALU_AND;
+          JR: begin
+            cuif.jumpreg = 1'b1;
+            cuif.regwr = 1'b1;
+          end
+          NOR: cuif.aluop = ALU_NOR;
+          OR: cuif.aluop = ALU_OR;
+          SLT: cuif.aluop = ALU_SLT;
+          SLTU: cuif.aluop = ALU_SLTU;
+          SLL: begin
+            cuif.shift = 1'b1;
+            cuif.aluop = ALU_SLL;
+          end
+          SRL: begin
+            cuif.shift = 1'b1;
+            cuif.aluop = ALU_SRL;
+          end
+          SUB, SUBU: cuif.aluop = ALU_SUB;
+          XOR: cuif.aluop = ALU_XOR;
+        endcase
+      end
+
+      /********* I-type *************/
+      ADDIU: begin
+        cuif.alusrc = 1'b1;
+        cuif.aluop = ALU_ADD;
+      end
+      ADDI: begin
+        // ???
+        cuif.alusrc = 1'b1;
+        cuif.aluop = ALU_ADD;
+        cuif.ext = 1'b0;
+      ANDI: begin
+        cuif.alusrc = 1'b1;
+        cuif.ext = 1'b0;
+        cuif.aluop = ALU_AND;
+      end
+      BEQ, BNE: begin
+        cuif.regwr = 1'b0;
+        cuif.branch = 1'b1;
+        cuif.aluop = ALU_SUB;
+      end
+      LUI: begin
+        cuif.lui = 1'b1;
+        cuif.alusrc = 1'b1;
+        cuif.ext = 1'b0;
+        cuif.aluop = ALU_OR;
+      end
+      LW: begin
+        cuif.memread = 1'b1;
+        cuif.alusrc = 1'b1;
+        cuif.memtoreg = 1'b1;
+        cuif.aluop = ALU_ADD;
+      end
+      ORI: begin
+        cuif.alusrc = 1'b1;
+        cuif.ext = 1'b0;
+        cuif.aluop = ALU_OR;
+      end
+      SLTI: begin
+        cuif.alusrc = 1'b1;
+        cuif.aluop = ALU_SLT
+      end
+      SLTIU: begin
+        cuif.alusrc = 1'b1;
+        cuif.aluop = ALU_SLTU;
+      end
+      SW: begin
+        cuif.memwrite = 1'b1;
+        cuif.regwrite = 1'b0;
+        cuif.alusrc = 1'b1;
+        cuif.aluop = ALU_ADD;
+      end
+      XORI: begin
+        cuif.alusrc = 1'b1;
+        cuif.ext = 1'b0;
+      end
+
+      /******** J-type ********/
+      J: begin
+        cuif.regwr = 1'b0;
+        cuif.jump = 1'b1;
+      end
+      JAL: begin
+        cuif.jumpal = 1'b1;
+        cuif.jump = 1'b1;
+      end
+
+      /******** Other **********/
+      HALT: begin
+        cuif.halt = 1'b1;
+        cuif.regwr = 1'b0;
+      end
+
+    endcase
+  end
 
 endmodule
