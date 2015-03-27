ced60d44317536cacf082711321ad2ef9aa495e0
Everett Berry <epberry@purdue.edu>
2015-03-27 04:45:44 -0400

    icache working

--- source/caches.sv
+++ source/caches.sv
@@ -12,7 +12,7 @@ module caches (
    parameter CPUID = 0;
    
    // icache
-   //icache  ICACHE(dcif, ccif);
+   icache  ICACHE(CLK, nRST, dcif, ccif);
    // dcache
    dcache  DCACHE(CLK, nRST, dcif, ccif);
 
@@ -20,17 +20,17 @@ module caches (
    //assign dcif.flushed = dcif.halt;
 
    //singlecycle
-   assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait : 0;
+   // assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait : 0;
    // assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~ccif.dwait : 0;
-   assign dcif.imemload = ccif.iload;
+   // assign dcif.imemload = ccif.iload;
    // assign dcif.dmemload = ccif.dload;
 
 
-   assign ccif.iREN = dcif.imemREN;
-   //assign ccif.dREN = dcif.dmemREN;
-   //assign ccif.dWEN = dcif.dmemWEN;
-   //assign ccif.dstore = dcif.dmemstore;
-   assign ccif.iaddr = dcif.imemaddr;
-   //assign ccif.daddr = dcif.dmemaddr;
+   // assign ccif.iREN = dcif.imemREN;
+   // assign ccif.dREN = dcif.dmemREN;
+   // assign ccif.dWEN = dcif.dmemWEN;
+   // assign ccif.dstore = dcif.dmemstore;
+   // assign ccif.iaddr = dcif.imemaddr;
+   // assign ccif.daddr = dcif.dmemaddr;
 
 endmodule
